699a5b0beb01ee7da0a928c02ae8e6535bb9be5f
孙秋云 <1186386839@qq.com>
2021-05-26 09:01:14 +0800

    '管理页面的主体搭建'

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDom from "react-dom";
 import App from "./App";
 import memoryUtils from "./utils/memoryUtils";
 import { getUser } from "./utils/storageUtils";
-console.log("1");
 let user = getUser();
 memoryUtils.user = user;
 
